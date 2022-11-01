2f1d7bf4c09c65432360dfd26477998c33eb470a
Connor <connormcq19@gmail>
2022-11-01 16:27:01 -0600

    just for fun

---
+++ practice/variables.js
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+
+// wow coding is funny 
+// thats so wierd......
+let x = 500;
+let y = 276;
+
+let sum = x + y
+let pi = 3.14;
+
+console.log("x is ", x);
+
+let name = "Connor McQuoston" ;
+
+console.log(WhatWillHappen, sum); 
+
+let crazMath = 100 * x / (sum + WhatWillHappen) - y; 
+
+let isGameOver = false; 
+
+console.log(isGameOver, typeof isGameOver); 
