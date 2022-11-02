510e83f3eff9a44a0660eb4701667df6fa6e9a39
Connor <connormcq19@gmail>
2022-11-02 16:08:44 -0600

    Added arrays and objects

--- practice/variables.js
+++ practice/variables.js
@@ -10,7 +10,13 @@ let pi = 3.14;
 
 console.log("x is ", x);
 
-let name = "Connor McQuoston" ;
+let myname = "Connor McQuoston" ;
+
+let whatWIllHappen = myname + x;
+
+// strings have a length property
+
+myname.length; 
 
 console.log(WhatWillHappen, sum); 
 
@@ -19,3 +25,23 @@ let crazMath = 100 * x / (sum + WhatWillHappen) - y;
 let isGameOver = false; 
 
 console.log(isGameOver, typeof isGameOver); 
+
+// arrays can hold a list of values 
+
+let ages = [25, 17, 16, 85];
+
+console.log("ages is an array", ages);
+
+let secondAge = ages[1];
+
+let student = {
+    name: "Toby"
+    grade: "A++"
+    gpa: 4.0,
+    wearsAHat: true, 
+    Friends: ["Jackson", "Lukas"]
+};
+console.log("student is", student);
+console.log("student name is", student.name);
+
+student.gradeLevel = 9;
